4a18891c23320b6693ba65806b532a67c36e6cfb
Coaixy <Coaixy@outlook.com>
2024-10-24 19:25:13 +0800

    新增排名显示

--- src/views/Rankings.vue
+++ src/views/Rankings.vue
@@ -5,39 +5,46 @@
         <div class="header-title" style="text-align: center">
           <el-text tag="b" size="large">排行榜TOP10</el-text>
         </div>
-        <el-divider class="common-divider" />
-        
+        <el-divider class="common-divider"/>
+
       </el-header>
       <el-main class="common-main">
         <div class="main-header">
           <el-text size="large">
             <el-icon>
-              <Avatar />
+              <Avatar/>
             </el-icon>
             您的昵称：{{ userData.nickName }}
           </el-text>
-          <el-divider direction="vertical" />
-          <el-button type="primary" @click="edit" link>修改</el-button>
+          <el-divider direction="vertical"/>
+          <el-button type="primary" @click="edit" link>修改昵称</el-button>
+          <br>
+         <el-text size="large">
+           <el-icon>
+             <GoldMedal/>
+           </el-icon>
+           您当前的排名：{{ rank }}
+         </el-text>
         </div>
         <!-- 表格 -->
         <div class="table-container">
           <el-table
-            :data="rankingsData.records"
-            :row-class-name="tableRowClassName"
-            v-loading="loading"
-            element-loading-text="数据加载中..."
+              :data="rankingsData.records"
+              :row-class-name="tableRowClassName"
+              v-loading="loading"
+              element-loading-text="数据加载中..."
           >
-            <el-table-column label="排名" type="index" align="center" width="80" fixed />
-            <el-table-column prop="nickName" label="昵称" align="center" fixed />
-            <el-table-column prop="totalCount" label="总次数" align="center" />
-            <el-table-column prop="averageSpeed" label="平均速度" align="center" />
+            <el-table-column label="排名" type="index" align="center" width="80" fixed/>
+            <el-table-column prop="nickName" label="昵称" align="center" fixed/>
+            <el-table-column prop="totalCount" label="总次数" align="center"/>
+            <el-table-column prop="averageSpeed" label="平均速度" align="center"/>
           </el-table>
           <el-text>注意：排名仅适用于已绑定用户</el-text>
         </div>
-        
+
       </el-main>
       <el-footer class="common-footer">
-        
+
         <el-button type="primary" plain @click="goHome">我的数据</el-button>
         <el-button type="primary" plain @click="goToday">今日数据</el-button>
         <el-button type="primary" plain @click="goBind">重新绑定</el-button>
@@ -48,7 +55,8 @@
 
     <!-- 修改昵称对话框 -->
     <el-dialog v-model="centerDialogVisible" title="修改昵称" width="300" align-center>
-      <el-input v-model="newNickName" style="width: 270px" maxlength="10" placeholder="请输入要修改的昵称" show-word-limit type="text" />
+      <el-input v-model="newNickName" style="width: 270px" maxlength="10" placeholder="请输入要修改的昵称"
+                show-word-limit type="text"/>
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="centerDialogVisible = false">取消</el-button>
@@ -60,11 +68,11 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted} from 'vue';
 import axios from 'axios';
 import router from '@/router';
-import { useRoute } from 'vue-router';
-import { ElMessage } from 'element-plus'
+import {useRoute} from 'vue-router';
+import {ElMessage} from 'element-plus'
 
 interface RankingsData {
   records: Array<{
@@ -74,6 +82,7 @@ interface RankingsData {
   }>;
   total: number;
 }
+
 // 用户数据
 interface UserData {
   openid: string
@@ -84,12 +93,14 @@ interface UserData {
   isPass: string
   nickName: string
 }
+
 const rankingsData = ref<RankingsData>({
   records: [],
   total: 0,
 });
 const pageNo = ref<number>(1);
 const pageSize = ref<number>(10);
+const rank = ref<number>(0)
 const userData = ref<UserData>({
   openid: '',
   sid: '',
@@ -129,7 +140,7 @@ const editNickName = async () => {
   try {
     const response = await axios.post(`http://coaixy.bluedog233.cn/front/change_nickname`, formData);
     if (response.data.code === 200) {
-      ElMessage({ message: '昵称修改成功', type: 'success' });
+      ElMessage({message: '昵称修改成功', type: 'success'});
       await getUserInfo();
       await fetchRankings();
       newNickName.value = '';
@@ -158,6 +169,23 @@ const goBind = () => {
   router.push(`/bind`)
 };
 
+
+const getUserRank = async () => {
+  try {
+    const timestamp = new Date().getTime();
+    const response = await axios.get(`http://coaixy.bluedog233.cn/front/rank_index?ts=${timestamp}`, {
+      params: {
+        uid: getUidFromURL(),
+        sign: getSignFromURL()
+      }
+    });
+    rank.value = response.data.data
+    console.log('用户排名:', response.data.data);
+  } catch (error) {
+    console.error('获取用户排名失败', error);
+  }
+}
+
 const fetchRankings = async (page: number = 1) => {
   pageNo.value = page;
   const timestamp = new Date().getTime();
@@ -185,9 +213,9 @@ const getUserInfo = async () => {
   }
 };
 const tableRowClassName = ({
-  row,
-  rowIndex,
-}: {
+                             row,
+                             rowIndex,
+                           }: {
   row: RankingsData['records']
   rowIndex: number
 }) => {
@@ -198,12 +226,13 @@ const tableRowClassName = ({
     return 'warning-row'
   } else if (rowIndex === 2) {
     return 'info-row'
-  } 
+  }
   return ''
 };
 onMounted(() => {
   fetchRankings();
   getUserInfo();
+  getUserRank();
 });
 </script>
 
@@ -211,23 +240,29 @@ onMounted(() => {
 .common-layout {
   height: 100vh;
 }
+
 .common-header {
   margin-top: 10px;
   padding: 0px;
 }
+
 .common-main {
   flex-grow: 1; /* 主区域占满剩余空间 */
 }
+
 .common-footer {
   text-align: center;
   margin-top: auto; /* 确保 footer 固定在最下面 */
 }
+
 .el-table .warning-row {
   --el-table-tr-bg-color: var(--el-color-warning-light-9);
 }
+
 .el-table .success-row {
   --el-table-tr-bg-color: var(--el-color-success-light-9);
 }
+
 .el-table .info-row {
   --el-table-tr-bg-color: var(--el-color-info-light-9);
 }
